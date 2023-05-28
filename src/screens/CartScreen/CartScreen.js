179caaa978e91bc19e6d7e2c6167fa7f2138579e
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-05-28 21:43:54 +0300

    toast container fix

--- src/screens/CartScreen/CartScreen.js
+++ src/screens/CartScreen/CartScreen.js
@@ -1,7 +1,7 @@
 import CartItem from 'components/CartItem/CartItem';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 
 import { submitOrderToServer } from 'utils/firebaseOperations';
